5e6c266dee574a64fcc965ba28ef5dd83e64b87c
airlaf <airlaf@andrew.cmu.edu>
2026-01-18 19:02:42 -0500

    New scene with plane and game object

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,15 +1,35 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 90f; // degrees per second
+    
+    public Move moveScript;
+    public GameObject thisIsAGameObject;
+    public Transform thisIsATransform;
+    public float rotationSpeed = 90f; // degrees per second
+    
+    /*
+    public List<float> thisIsAListOfFloats = new List<float>();
+
+    public float moveSpeed = 10f;
+
+    public bool thisIsABoolean = true;
+
+    public string thisIsAString = "Hello, World!";
+    */
+    
     
     void Update()
     {
-        // Rotate the cube around its Y-axis (vertical spin)
-        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
-        
-        // Alternatively, for rotation on all axes:
-        // transform.Rotate(rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime, rotationSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            moveScript.MoveMe();
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            thisIsATransform.Translate(1*Time.deltaTime, 0, 0);
+        }
     }
 }
